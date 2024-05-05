cf7d82ccdc846f4b98a1992790c6c1f2becd9c8a
Smeshnou <girilukvladislav@gmail.com>
2024-05-05 19:22:13 +0300

    Solved task4 chapter3.3

---
+++ Practice/Chapter3.3/Task4/src/com/task3_3_42/RedBlackBST.java
@@ -0,0 +1,350 @@
+package com.task3_3_42;
+
+import java.util.Random;
+
+public class RedBlackBST<K extends Comparable<K>, V> {
+    private Node root;
+    Node lastAccessedNode = null;  
+    
+    int redCount = 0;
+    int blackCount = 0;
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+    private class Node{
+        private K key;
+        private V val;
+        private Node left;
+        private Node right;
+        private int size;
+        private boolean color;
+        public Node(K key, V val, int size, boolean color){
+            this.key = key;
+            this.val = val;
+            this.size = size;
+            this.color = color;
+        }
+    }
+
+    public double redProcent(){
+        blackCount = 0;
+        redCount = 0;
+        redProcent(root);
+        return (double)redCount/blackCount * 100;
+    }
+
+    private void redProcent(Node node){
+        if(node == null){
+            ++blackCount;
+            return;
+        }
+        if(node.color){
+            ++redCount;
+        }
+        else{
+            ++blackCount;
+        }
+        redProcent(node.left);
+        redProcent(node.right);
+    }
+
+    public boolean contains(K key) {
+        return get(key) != null;
+    }
+
+    public void delete(K key){
+        if (!contains(key)) return;
+        if (!isRed(root.left) && !isRed(root.right)){
+            root.color = RED;
+        }
+        root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
+    }
+    
+    private Node delete(Node node, K key){
+
+        if (key.compareTo(node.key) < 0){
+            if (!isRed(node.left) && !isRed(node.left.left)){
+                node = moveRedLeft(node);
+            }
+            node.left = delete(node.left, key);
+        }
+        else {
+            if (isRed(node.left)){
+                node = rotateRight(node);
+            }
+            if (key.compareTo(node.key) == 0 && (node.right == null)){
+                return null;
+            }
+            if (!isRed(node.right) && !isRed(node.right.left)){
+                node = moveRedRight(node);
+            }
+            if (key.compareTo(node.key) == 0) {
+                node.val = get(node.right, min(node.right).key).val;
+                node.key = min(node.right).key;
+                node.right = deleteMin(node.right);
+            }
+            else{
+                node.right = delete(node.right, key);
+            }
+        }
+        return balance(node);
+    }
+
+    public void deleteMax(){
+        if (!isRed(root.left) && !isRed(root.right)){
+            root.color = RED;
+        }
+        root = deleteMax(root);
+        if (!isEmpty()){ 
+            root.color = BLACK;
+        }
+    }
+
+    private Node deleteMax(Node h){
+        if (isRed(h.left)){
+            h = rotateRight(h);
+        }
+        if (h.right == null){
+            return null;
+        }
+        if (!isRed(h.right) && !isRed(h.right.left)){
+            h = moveRedRight(h);
+        }
+        h.right = deleteMax(h.right);
+
+        return balance(h);
+    }
+
+    public void deleteMin(){
+        if (!isRed(root.left) && !isRed(root.right)){
+            root.color = RED;
+        }
+        root = deleteMin(root);
+        if (!isEmpty()){
+            root.color = BLACK;
+        }
+    }
+
+    private Node deleteMin(Node node){
+        if (node.left == null){
+            return null;
+        }
+        if (!isRed(node.left) && !isRed(node.left.left)){
+            node = moveRedLeft(node);
+        }
+        node.left = deleteMin(node.left);
+        return balance(node);
+    }
+
+    private Node balance(Node node){
+        if (isRed(node.right) && !isRed(node.left)){
+            node = rotateLeft(node);
+        }
+        if (isRed(node.left) && isRed(node.left.left)){
+             node = rotateRight(node);
+        }
+        if (isRed(node.left) && isRed(node.right)){
+            flipColors(node);
+        }     
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    private Node moveRedRight(Node h){
+        flipColors(h);
+        if (isRed(h.left.left)) {
+            h = rotateRight(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    private Node moveRedLeft(Node h){
+        flipColors(h);
+        if (isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    public boolean isEmpty(){
+        return isEmpty(root);
+    }
+
+    public boolean isEmpty(Node node){
+        return node == null;
+    }
+
+    private boolean isRed(Node node){
+        if(node == null){
+            return false;
+        }
+        else{
+            return node.color == RED;
+        }
+    }
+
+    private Node rotateLeft(Node oldRoot){
+        Node newRoot = oldRoot.right;
+        oldRoot.right = newRoot.left;
+        newRoot.left = oldRoot;
+        newRoot.color = oldRoot.color;
+        oldRoot.color = RED;
+        newRoot.size = oldRoot.size;
+        oldRoot.size = 1 + size(oldRoot.left) + size(oldRoot.right);
+        return newRoot;
+    } 
+
+    private Node rotateRight(Node oldRoot){
+        Node newRoot = oldRoot.left;
+        oldRoot.left = newRoot.right;
+        newRoot.right = oldRoot;
+        newRoot.color = oldRoot.color;
+        oldRoot.color = RED;
+        newRoot.size = oldRoot.size;
+        oldRoot.size = 1 + size(oldRoot.left) + size(oldRoot.right);
+        return newRoot;
+    }
+
+    private void flipColors(Node node){
+        node.color = RED;
+        node.left.color = BLACK;
+        node.right.color = BLACK;
+    }
+
+    public int size(){
+        return size(root);
+    }
+
+    private int size(Node node){
+        if(node == null) {
+            return 0;
+        }
+        else{
+            return node.size;
+        }
+    }
+
+    public V get(K key){
+        if(lastAccessedNode != null && lastAccessedNode.key == key){
+            return lastAccessedNode.val;
+        }
+        else{
+            Node node = get(root, key);
+            if(node != null){
+                return node.val;
+            }
+            else{
+                return null;
+            }
+        }
+    }
+
+    private Node get(Node node, K key){
+        if(node == null){
+            return null;
+        }
+        while(key != node.key){
+            if(key.compareTo(node.key) > 0){
+                node = node.right;
+            }
+            else if(key.compareTo(node.key) < 0){
+                node = node.left;
+            }
+            
+            if(node == null){
+                return null;
+            }
+        }
+        lastAccessedNode = node;
+        return node;
+    }
+
+    public void put(K key, V val){
+        if(lastAccessedNode != null && lastAccessedNode.key == key){
+            lastAccessedNode.val = val;
+        }
+        else{
+            root = put(root, key, val);
+            root.color = BLACK;
+        }
+    }
+
+    private Node put(Node node, K key, V val){
+        if(node == null){ 
+            lastAccessedNode = new Node(key, val, 1, RED); 
+            return lastAccessedNode;
+        }
+        int compare = key.compareTo(node.key);
+        if(compare < 0){
+            node.left = put(node.left, key, val);
+        }
+        else if(compare > 0){
+            node.right = put(node.right, key, val);
+        }
+        else{
+            node.val = val;
+        }
+
+        if(isRed(node.right) && !isRed(node.left)){
+            node = rotateLeft(node);
+        }
+        if(isRed(node.left) && isRed(node.left.left)){
+            node = rotateRight(node);
+        }
+        if(isRed(node.left) && isRed(node.right)){
+            flipColors(node);
+        }
+
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public K min(){
+        if(root == null){
+            return null;
+        }
+        return min(root).key;
+    }
+
+    private Node min(Node node){
+        if(node == null){
+            return null;
+        }
+        while(node.left != null){
+            node = node.left;
+        }
+        lastAccessedNode = node;
+        return node;
+    }
+
+    public K max(){
+        if(root == null){
+            return null;
+        }
+        return max(root).key;
+    }
+
+    private Node max(Node node){
+        if(node == null){
+            return null;
+        }
+        while(node.right != null){
+            node = node.right;
+        }
+        lastAccessedNode = node;
+        return node;
+    }
+
+    public static void main(String[] args){
+        RedBlackBST<Integer, String> tree = new RedBlackBST<>();
+        Random random = new Random();
+        for(int i = 0; i < 10000; i++){
+            tree.put(random.nextInt(10000), "a");
+        }
+        System.out.println(tree.redProcent());
+    }
+}
