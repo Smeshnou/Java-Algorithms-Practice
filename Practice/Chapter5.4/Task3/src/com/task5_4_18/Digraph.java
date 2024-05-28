1a6ddb02c82c27ea7b78cf3b24a6797f3bd81160
Smeshnou <girilukvladislav@gmail.com>
2024-05-28 22:00:12 +0300

    Solved Chapter5.4

---
+++ Practice/Chapter5.4/Task3/src/com/task5_4_18/Digraph.java
@@ -0,0 +1,50 @@
+package com.task5_4_18;
+
+import java.util.ArrayList;
+
+public class Digraph {
+
+    private int vertices;           
+    private int edges;                 
+    private ArrayList<ArrayList<Integer>> adj;    
+    private ArrayList<Integer> indegree;        
+
+    public Digraph(int vertices) {
+        this.vertices = vertices;
+        this.edges = 0;
+        indegree = new ArrayList<>(vertices);
+        for (int v = 0; v < vertices; v++) {
+            indegree.add(0);
+        }
+        adj = new ArrayList<>(vertices);
+        for (int v = 0; v < vertices; v++) {
+            adj.add(new ArrayList<>(vertices));
+        }
+    }
+
+    public int vertices() {
+        return vertices;
+    }
+
+    public int edges() {
+        return edges;
+    }
+
+    public void addEdge(int v, int w) {
+        adj.get(v).add(w);
+        indegree.set(w, indegree.get(w)+1);
+        edges++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj.get(v);
+    }
+
+    public int outdegree(int v) {
+        return adj.get(v).size();
+    }
+
+    public int indegree(int v) {
+        return indegree.get(v);
+    }
+}
---
+++ Practice/Chapter5.4/Task4/src/com/task5_4_19/NFA.java
@@ -0,0 +1,107 @@
+package com.task5_4_19;
+
+import java.util.ArrayList;
+
+public class NFA {
+
+    private Digraph graph;
+    private String regexp;
+    private int length;
+
+    public NFA(String regexp) {
+        for(int i = 0; i < regexp.length(); i++){
+            if(regexp.charAt(i) == '['){
+                int j = i + 1;
+                StringBuilder str = new StringBuilder();
+                str.append('(');
+                str.append(regexp.charAt(j));
+                j++;
+                while(regexp.charAt(j) != ']'){
+                    str.append('|');
+                    str.append(regexp.charAt(j));
+                    j++;
+                }
+                str.append(')');
+                regexp = regexp.substring(0, i) + str.toString() + regexp.substring(j + 1, regexp.length());
+                this.regexp = regexp;
+            }
+      }
+        this.regexp = regexp;
+        length = regexp.length();
+        Stack<Integer> ops = new Stack<>();
+        graph = new Digraph(length+1);
+        for (int i = 0; i < length; i++) {
+            int lp = i;
+            
+            
+            if (regexp.charAt(i) == '(' || regexp.charAt(i) == '|'){
+                ops.push(i);
+            }
+            else if (regexp.charAt(i) == ')') {
+                ArrayList<Integer> or = new ArrayList<>();
+                or.add(ops.pop());
+                for(int j = 0; regexp.charAt(or.get(j)) != '('; j++){
+                    or.add(ops.pop());
+                    graph.addEdge(or.get(j), i);
+                }
+                lp = or.getLast();
+                for(int j = 0; j < or.size(); j++){
+                    graph.addEdge(lp, or.get(j)+1);
+                }
+            }
+
+            if (i < length-1 && regexp.charAt(i+1) == '*') {
+                graph.addEdge(lp, i+1);
+                graph.addEdge(i+1, lp);
+            }
+            if (i < length-1 && regexp.charAt(i+1) == '+') {
+                graph.addEdge(i+1, lp);
+            }
+            if (regexp.charAt(i) == '(' || regexp.charAt(i) == '*' || regexp.charAt(i) == ')'|| regexp.charAt(i) == '+'){
+                graph.addEdge(i, i+1);
+            }
+        }
+    }
+    
+    public boolean recognizes(String txt) {
+        DirectedDFS dfs = new DirectedDFS(graph, 0);
+        ArrayList<Integer> pc = new ArrayList<>();
+        for (int v = 0; v < graph.vertices(); v++){
+            if (dfs.marked(v)){ 
+                pc.add(v);
+            }
+        }
+        for (int i = 0; i < txt.length(); i++) {
+            ArrayList<Integer> match = new ArrayList<>();
+            for (int v : pc) {
+                if (v < length){
+                    if ((regexp.charAt(v) == txt.charAt(i)) || regexp.charAt(v) == '.'){
+                        match.add(v+1);
+                    }
+                }
+            }
+            dfs = new DirectedDFS(graph, match);
+            pc = new ArrayList<>();
+            for (int v = 0; v < graph.vertices(); v++){
+                if (dfs.marked(v)){ 
+                    pc.add(v);
+                }
+            }
+        }
+
+        for (int v : pc){
+            if (v == length){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String regexp = "(ABG+[ABC])";
+        String txt = "ABG";
+        NFA nfa = new NFA(regexp);
+        System.out.println(nfa.recognizes(txt));
+    }
+
+}
